d58ea40980e1153b9ed5bee2663275231d3bc392
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-09-25 16:20:00 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from '../../images/Logo.svg';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            <img src={Image} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/about">About</a>
+                <a href="/Blog">Blog</a>
+                <a href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
